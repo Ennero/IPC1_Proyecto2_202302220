08fc13005428f2f0f2c977da03a9fb18f2a455c4
ElEnnero <159963896+ElEnnero@users.noreply.github.com>
2024-04-13 22:39:11 -0600

    Cabios pequeños

--- Backend/indice.js
+++ Backend/indice.js
@@ -1,26 +1,46 @@
+//Las Dependencias
+const fs=require('fs');
 const express = require('express');
 const bodyParser = require("body-parser");
 const cors= require('cors');
 
+//Las variables de los servidor, el puerto y el
+//archivo para no tener que meter todo a cada rato
 const servidor=express();
 const puerto= 5000;
+const archivito='usuarios.json'
 
+//uso bodyParser y los cors
 servidor.use(bodyParser.json())
 servidor.use(cors());
 
-//Aquí uso el array como una base de datos
+//Aquí uso el array como una base de datos super sofisticada
 let datos=[];
+
+//Aquí verifico la existencia del archivo y actualizo la base de datos
+if (!fs.existsSync(archivito)){
+    fs.writeFileSync(archivito, JSON.stringify(datos))
+}else{
+    const serivicio= fs.readFileSync(archivito,'utf-8')
+    datos=JSON.parse(serivicio)
+}
+
+//Función para actualizar el archivito
+function actualizar(){
+    fs.writeFileSync(archivito,JSON.stringify(datos))
+}
+
 
 //Aquí hago el endpoint
 servidor.get("/", (req,res) =>{
     let saludo ={
-        Saludo: "Hola Mundo"
+        Saludo: "Funcionando..."
     };
     res.json(saludo);
 }
 );
 
-//Aquí obtengo los datos de la lista
+//Aquí retorno los datos de la lista
 servidor.get("/students", (req,res) =>{
     res.json(datos)
 
